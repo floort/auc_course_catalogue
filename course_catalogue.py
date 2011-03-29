6fcc5fa1bbadea3115659525904d160d5c21744c
Floor Terra <floort@gmail.com>
2011-03-29 16:41:02 +0200

    Bugfix

--- course_catalogue.py
+++ course_catalogue.py
@@ -2,13 +2,23 @@
 
 import sys
 import subprocess
+import StringIO
+from lxml import html
+
 
 def pdf_to_html(filename):
-    p = subprocess.Popen(["pdftotext", "-layout", "-htmlmeta", filename, "-"])
-    html = p.read()
-    p.terminate()
-    return html
+    """Convert a pdf file on disk to html.
+    Needs the pdftotext binary from the cpdf package."""
+    p = subprocess.Popen(["pdftotext", "-layout", "-htmlmeta", filename, "-"],
+        stdout=subprocess.PIPE)
+    return p.communicate()[0]
+
+def parse_courses(html_doc):
+    tree = html.fromstring(html_doc)
+    return tree
 
 
 if __name__ == "__main__":
-    print pdf_to_html(sys.argv[1])
+    html_doc = pdf_to_html(sys.argv[1])
+    print dir(parse_courses(html_doc))
+
