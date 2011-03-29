c5b86422d10662203e568b687321eeb019850e65
Floor Terra <floort@gmail.com>
2011-03-29 17:07:11 +0200

    Parse course list (with lots of errors)

--- course_catalogue.py
+++ course_catalogue.py
@@ -2,23 +2,35 @@
 
 import sys
 import subprocess
-import StringIO
-from lxml import html
 
 
-def pdf_to_html(filename):
-    """Convert a pdf file on disk to html.
+def pdf_to_txt(filename):
+    """Convert a pdf file on disk to text.
     Needs the pdftotext binary from the cpdf package."""
-    p = subprocess.Popen(["pdftotext", "-layout", "-htmlmeta", filename, "-"],
+    p = subprocess.Popen(["pdftotext", "-layout", filename, "-"],
         stdout=subprocess.PIPE)
     return p.communicate()[0]
 
-def parse_courses(html_doc):
-    tree = html.fromstring(html_doc)
-    return tree
+def parse_course_list(doc):
+    course_list = []
+    doc = doc[:doc.find("Description of Courses in the Academic Core")]
+    doc = doc[doc.find("ACC"):]
+    doc = doc.split("\n")
+    for l in doc:
+        course = l.split()
+        if len(course) < 2:
+            continue
+        if course[-2][-1] == ".":
+            course = [course[0], " ".join(course[1:-1]).strip(".")]
+        course_list.append(course)
+    return course_list
+
+
+def parse_full(doc):
+    courses = parse_course_list(doc)
+    return courses
 
 
 if __name__ == "__main__":
-    html_doc = pdf_to_html(sys.argv[1])
-    print dir(parse_courses(html_doc))
-
+    doc = pdf_to_txt(sys.argv[1])
+    print parse_full(doc)
