bccad0ea34e376623efd5cd0c5b419d856b28c04
Floor Terra <floort@gmail.com>
2011-03-30 02:32:59 +0200

    Start using regexps

--- course_catalogue.py
+++ course_catalogue.py
@@ -2,7 +2,7 @@
 
 import sys
 import subprocess
-
+import re
 
 def pdf_to_txt(filename):
     """Convert a pdf file on disk to text.
@@ -12,18 +12,7 @@ def pdf_to_txt(filename):
     return p.communicate()[0]
 
 def parse_course_list(doc):
-    course_list = []
-    doc = doc[:doc.find("Description of Courses in the Academic Core")]
-    doc = doc[doc.find("ACC"):]
-    doc = doc.split("\n")
-    for l in doc:
-        course = l.split()
-        if len(course) < 2:
-            continue
-        if course[-2][-1] == ".":
-            course = [course[0], " ".join(course[1:-1]).strip(".")]
-        course_list.append(course)
-    return course_list
+    return re.findall(r'^(?P<id>(?:SCI|SSC|HUM|ACC)\S+)\s+(?P<name>\S*)',doc,re.M)
 
 
 def parse_full(doc):
